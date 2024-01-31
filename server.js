617afa678a47fdb7e3f9b68ca112165c7ad0410b
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2024-01-31 14:51:19 +0530

    Route changes

--- server.js
+++ server.js
@@ -8,13 +8,30 @@ app.use(express.static(__dirname+"/public"));
 app.use(parser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
+  // Get index Page
 app.get("/", (req,res)=>{
     res.render("index")
 })
 
+// Get the page for find user by id 
 app.get("/findPage", (req,res)=>{
     res.render("findUser")
  })
+
+//  Finds a user with id and renders all the user data
+
+
+// Delete a user
+
+
+//  Get update page for  user
+
+
+//  Add user
+
+
+// Update a user
+
 
 app.listen(80, ()=>{
     console.log("Server is running at port 80");
